6bcd510b3702887431c2f5a0615663face6bb8f9
Neha S <neha211205@gmail.com>
2026-06-22 00:51:32 +0530

    Add files via upload

---
+++ Module-2-DSA/Linear_search.py
@@ -0,0 +1,27 @@
+#linear search
+
+
+def search(arr, x):
+    n = len(arr)
+    for i in range(0, n):
+        if (arr[i] == x):
+            return i
+    return -1
+if __name__ == "__main__":
+    arr = [2, 3, 4, 10, 40]
+    x = 10
+    result = search(arr, x)
+    if(result == -1):
+        print("Element is not present in array")
+    else:
+        print("Element is present at index", result)
+
+
+
+# Linear Search
+# Time Complexity:
+# Best Case    : O(1)
+# Average Case : O(n)
+# Worst Case   : O(n)
+# Space Complexity : O(1)
+# Auxiliary Space  : O(1)
